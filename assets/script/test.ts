d920d5f907d2061d1616a2cb719e8b477ec0bc66
sunym <sunym02@mail.jj.cn>
2019-09-23 21:29:12 +0800

    1.添加三次贝塞尔曲线

--- assets/script/test.ts
+++ assets/script/test.ts
@@ -23,10 +23,11 @@ export default class Test extends cc.Component {
         this.clearDraw()
         this.drawLine(cc.v2(0, 0), cc.v2(300, 300));
         this.drawPoint();
+        this.drawBezierCurve(cc.v2(0, 0), cc.v2(100, 0), cc.v2(0,200), cc.v2(400, 100));
     }
     drawPoint() {
+        this.graphics.strokeColor.fromHEX("#ff0000");
         this.graphics.fillColor.fromHEX("#ff0000");
-        // = cc.Color.YELLOW;
         this.graphics.circle(300, 300, 6);
         this.graphics.stroke();
         this.graphics.fill();
@@ -41,9 +42,20 @@ export default class Test extends cc.Component {
         this.graphics.moveTo(p0.x, p0.y);
         this.graphics.lineTo(p1.x, p1.y);
         this.graphics.stroke();
-        this.graphics.fill();
     }
 
+    /**
+     * 绘制三次贝塞尔曲线
+     * @param p0 起点
+     * @param p1 控制点1
+     * @param p2 控制点2
+     * @param p3 终点
+     */
+    drawBezierCurve(p0: cc.Vec2, p1: cc.Vec2, p2:  cc.Vec2, p3:  cc.Vec2) {
+        this.graphics.moveTo(p0.x, p0.y);
+        this.graphics.bezierCurveTo(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+        this.graphics.stroke();
+    }
 
     clearDraw() {
         this.graphics.clear();
