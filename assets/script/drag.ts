19be221386b00fc7e148fe4f3c5c51371ee60c07
sunym <sunym02@mail.jj.cn>
2019-09-24 21:19:48 +0800

    1.拖拽组件

---
+++ assets/script/drag.ts
@@ -0,0 +1,25 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    getWorldPos(): cc.Vec2 {
+        let nodePos = this.node.convertToWorldSpaceAR(cc.Vec2.ZERO);
+        let v2 = cc.v2(nodePos.x, nodePos.y);
+        return v2;
+    }
+
+    setWorldPos(pos: cc.Vec2) {
+        let nodePos = this.node.parent.convertToNodeSpaceAR(pos);
+        let v2 = cc.v2(nodePos.x, nodePos.y);
+        this.node.position = v2;
+    }
+
+    onLoad() {
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, (touchEvent:cc.Event.EventTouch)=>{
+            let location = touchEvent.getDelta();
+            this.node.position = cc.v2(this.node.x + location.x, this.node.y + location.y);
+        })
+    }
+
+}
--- assets/script/test.ts
+++ assets/script/test.ts
@@ -7,8 +7,6 @@ export default class Test extends BaseDraw {
     start () {
         this.clearDraw()
         this.drawLine(cc.v2(0, 0), cc.v2(-300, -300), cc.Color.GREEN, 2);
-        this.setLineCap(cc.Graphics.LineCap.BUTT);
-        this.setLineJoin(cc.Graphics.LineJoin.MITER);
         this.drawCirclePoint(cc.v2(100, 10), cc.Color.RED, 6, true);
         this.drawCirclePoint(cc.v2(100, 40), cc.Color.GREEN, 6, false);
         this.drawRectPoint(cc.v2(100, 80), cc.Color.RED, 7, true);
